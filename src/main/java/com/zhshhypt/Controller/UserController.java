aa5a12593613006d4750739ee2bd1292c2621918
cyz <308218935@qq.com>
2021-05-30 17:56:14 +0800

    修改了用户添加信息

--- src/main/java/com/zhshhypt/Controller/UserController.java
+++ src/main/java/com/zhshhypt/Controller/UserController.java
@@ -3,11 +3,17 @@ package com.zhshhypt.Controller;
 
 import com.zhshhypt.pojo.User;
 import com.zhshhypt.service.IUserService;
+import com.zhshhypt.util.BaseControl;
+import com.zhshhypt.util.RETCODE;
+import net.sf.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * <p>
@@ -19,7 +25,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseControl {
     @Resource
     private IUserService iUserService;
 
@@ -41,15 +47,24 @@ public class UserController {
         User user = iUserService.selectById("23432");
         return user;
     }
-    
-     @RequestMapping(value ="/InserLine")
+    /**
+     * @Description:用户添加信息
+     * @params: [response, request]
+     * @return: void
+     * @Author: Administrator
+     * @Date: 2021/5/30 0030
+     */
+    @RequestMapping(value ="/InserLine")
     public void InsertData(HttpServletResponse response, HttpServletRequest request){
         JSONObject ret=new JSONObject();
         User user= null;
         try {
             user = new User();
-            
-            iTeachService.insert(user);
+            user.setId(UUID.randomUUID().toString().replaceAll("-",""));
+            user.setName("cyz");
+            user.setAge("25");
+            user.setAddress("兰州");
+            iUserService.insert(user);
             ret.put("code", RETCODE.R200);
             ret.put("msg","插入成功...");
         } catch (Exception e) {
@@ -57,10 +72,7 @@ public class UserController {
             ret.put("code",RETCODE.R400);
             ret.put("msg",e.getMessage());
         }
-
         write(response,ret.toString());
-
-
     }
 
 }
